99b4e6731fbe0ffdc735de860a9471860906e840
jaydemoody <jaydemoody81@gmail.com>
2021-08-21 19:36:36 +1000

    impletmented the coding for movement audio
    with the audio i imported into unity, i then coded the player.cs so that the jump sound would play on every jump. i decided that it would be best if i kept the pitch the same. i also decided to remove the log jump audio as i thought it might complicate things later on

--- GPR103_A4_J_Moody_2D_Game/Assets/Player.cs
+++ GPR103_A4_J_Moody_2D_Game/Assets/Player.cs
@@ -16,6 +16,15 @@ public class Player : MonoBehaviour
     public bool playerIsAlive = true; //Is the player currently alive?
     public bool playerCanMove = false; //Can the player currently move?
 
+    // this is the public section thingy-mah-bob where the audio for the game is help
+
+    public AudioClip jumpAudio;    
+    public AudioClip ScoreAudio;
+    public AudioClip hurtAudio;
+    
+
+
+
     private GameManager myGameManager; //A reference to the GameManager in the scene.
 
     // Start is called before the first frame update
@@ -30,18 +39,22 @@ public class Player : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.UpArrow) && transform.position.y < myGameManager.levelConstraintTop) //this code means that the player can move within a certain area, but the constraints in the game manager are implemented
         {
             transform.Translate(new Vector2(0, 1));
+            GetComponent<AudioSource>().PlayOneShot(jumpAudio);
         }
         else if (Input.GetKeyDown(KeyCode.DownArrow) && transform.position.y > myGameManager.levelConstraintBottom)// the "else if" stops the player from being able to move diagonally and pressing multiple directional keys simutainously
         {
             transform.Translate(new Vector2(0, -1));
+            GetComponent<AudioSource>().PlayOneShot(jumpAudio);
         }
         else if (Input.GetKeyDown(KeyCode.LeftArrow) && transform.position.x > myGameManager.levelConstraintLeft)
         {
             transform.Translate(new Vector2(-1, 0));
+            GetComponent<AudioSource>().PlayOneShot(jumpAudio);
         }
         else if (Input.GetKeyDown(KeyCode.RightArrow) && transform.position.x < myGameManager.levelConstraintRight)
         {
             transform.Translate(new Vector2(1, 0)); // I decided to use the "transform.translate" method as it meant that i was only dealing with vector 2 and i honestly thought it made the code look cleaner
+            GetComponent<AudioSource>().PlayOneShot(jumpAudio);
         }
     }
     
