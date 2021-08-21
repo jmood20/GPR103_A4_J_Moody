f19ea98eee82ac83f9fbcc8b9fd193171c532682
jaydemoody <jaydemoody81@gmail.com>
2021-08-21 22:21:03 +1000

    refining the kill function
    i have altered and refined the code so that when the player dies. all controls stop

--- GPR103_A4_J_Moody_2D_Game/Assets/Player.cs
+++ GPR103_A4_J_Moody_2D_Game/Assets/Player.cs
@@ -14,7 +14,7 @@ public class Player : MonoBehaviour
     public int playerLivesRemaining = 10; //PLayers actual lives remaining.
    
     public bool playerIsAlive = true; //Is the player currently alive?
-    public bool playerCanMove = false; //Can the player currently move?
+    public bool playerCanMove = true; //Can the player currently move?
 
     // this is the public section thingy-mah-bob where the audio for the game is help
 
@@ -35,31 +35,47 @@ public class Player : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.UpArrow) && transform.position.y < myGameManager.levelConstraintTop) //this code means that the player can move within a certain area, but the constraints in the game manager are implemented
+    { if (playerIsAlive == true)
         {
-            transform.Translate(new Vector2(0, 1));
-            GetComponent<AudioSource>().PlayOneShot(jumpAudio);
-        }
-        else if (Input.GetKeyDown(KeyCode.DownArrow) && transform.position.y > myGameManager.levelConstraintBottom)// the "else if" stops the player from being able to move diagonally and pressing multiple directional keys simutainously
-        {
-            transform.Translate(new Vector2(0, -1));
-            GetComponent<AudioSource>().PlayOneShot(jumpAudio);
-        }
-        else if (Input.GetKeyDown(KeyCode.LeftArrow) && transform.position.x > myGameManager.levelConstraintLeft)
-        {
-            transform.Translate(new Vector2(-1, 0));
-            GetComponent<AudioSource>().PlayOneShot(jumpAudio);
-        }
-        else if (Input.GetKeyDown(KeyCode.RightArrow) && transform.position.x < myGameManager.levelConstraintRight)
-        {
-            transform.Translate(new Vector2(1, 0)); // I decided to use the "transform.translate" method as it meant that i was only dealing with vector 2 and i honestly thought it made the code look cleaner
-            GetComponent<AudioSource>().PlayOneShot(jumpAudio);
+            if (Input.GetKeyDown(KeyCode.UpArrow) && transform.position.y < myGameManager.levelConstraintTop) //this code means that the player can move within a certain area, but the constraints in the game manager are implemented
+            {
+                transform.Translate(new Vector2(0, 1));
+                GetComponent<AudioSource>().PlayOneShot(jumpAudio);
+            }
+            else if (Input.GetKeyDown(KeyCode.DownArrow) && transform.position.y > myGameManager.levelConstraintBottom)// the "else if" stops the player from being able to move diagonally and pressing multiple directional keys simutainously
+            {
+                transform.Translate(new Vector2(0, -1));
+                GetComponent<AudioSource>().PlayOneShot(jumpAudio);
+            }
+            else if (Input.GetKeyDown(KeyCode.LeftArrow) && transform.position.x > myGameManager.levelConstraintLeft)
+            {
+                transform.Translate(new Vector2(-1, 0));
+                GetComponent<AudioSource>().PlayOneShot(jumpAudio);
+            }
+            else if (Input.GetKeyDown(KeyCode.RightArrow) && transform.position.x < myGameManager.levelConstraintRight)
+            {
+                transform.Translate(new Vector2(1, 0)); // I decided to use the "transform.translate" method as it meant that i was only dealing with vector 2 and i honestly thought it made the code look cleaner
+                GetComponent<AudioSource>().PlayOneShot(jumpAudio); //I decided to keep the audio the same pitch as i felt like it was cuter, i was also starting to get pressed for time
+            }
         }
     }
     void OnTriggerEnter2D(Collider2D collision)
     {
-        print("test hit");
+        if (collision.transform.GetComponent<Vehicle>() != null) //this is the collision code for the "highway" section of the game. the vehicle component was chosen as the component as it is the common demoninator amongst all the cars.
+        {
+            PlayerDeath();
+        }
     }
 
-}
+    void PlayerDeath()// this is a function of code that would be used to contain and initialise the code for the player to die, kind of like a tupperware container. 
+    {
+        GetComponent<AudioSource>().PlayOneShot(hurtAudio);
+        playerIsAlive = false;
+        playerCanMove = false;
+        print("oooof!");
+    }
+        
+        
+        
+        
+ }
